52224335894bc0ff898adcb6d2a9717784454ed4
Vikash Thakur <thakvikash622@gmail.com>
2024-02-29 12:21:21 +0530

    Learn better way to setproperties to the ele using for loop:

--- CustomReact/customReact.jsx
+++ CustomReact/customReact.jsx
@@ -1,9 +1,21 @@
-function customRender(reactElement, container) {
-    let ele = document.createElement(reactElement.type);
-    ele.innerHTML = reactElement.children;
-    ele.setAttribute('href', reactElement.props.href);
-    ele.setAttribute('target', reactElement.props.target);
-    container.appendChild(ele);
+// function customRender(reactElement, container) {
+//     let ele = document.createElement(reactElement.type);
+//     ele.innerHTML = reactElement.children;
+//     ele.setAttribute('href', reactElement.props.href);
+//     ele.setAttribute('target', reactElement.props.target);
+//     container.appendChild(ele);
+// }
+
+
+// In this funciton I don't need to select all properties one by one simply applying for loop to handle all the properties in props
+function customRender(reactElement, maincontainer){
+    var component = document.createElement(reactElement.type);
+    component.innerHTML = reactElement.children;
+    for(var prop in reactElement.props) {
+        if(prop == 'children') continue;
+        component.setAttribute(prop, reactElement.props[prop]);
+    }
+    maincontainer.appendChild(component);
 }
 
 const reactElement = {
