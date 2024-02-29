f95c7be20cbeb9eaeb9dada27de1f70fa049627f
Vikash Thakur <thakvikash622@gmail.com>
2024-02-29 16:48:21 +0530

    Learned about hook

--- 02Counter/src/App.jsx
+++ 02Counter/src/App.jsx
@@ -4,18 +4,27 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  let value = 15;
+  let [value, setvalue] = useState(12)
   let click = () => {
-    console.log("Values added", new Date())
+    console.log("Values added", value)
+    setvalue(value + 1)
   }
+
+  let remove = () => {
+    if(value > 0){
+      console.log("Removed: ", value)
+      setvalue(value - 1)
+    }
+  }
+  
 
   return (
     <>
     <h1>Welcome to the Webpage</h1>
     <h2>Counter Value: {value}</h2>
-    <button onClick={click}>Click Here to get result</button>
+    <button onClick={click}>Add value {value}</button>
     <br />
-    <button>Submit</button>
+    <button onClick={remove}>Remove value {value}</button>
     </>
   )
 }
