9b63b47c1664caf94533ef612f9c146a96eb0e12
Vikash Thakur <thakvikash622@gmail.com>
2024-03-01 00:59:49 +0530

    Imported a card and basic changes

--- 03TailwindProps/src/App.jsx
+++ 03TailwindProps/src/App.jsx
@@ -8,26 +8,27 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h2 className='bg-green-500 text-black p-4 rounded-xl'>Welcome to my Webpage:</h2>
+      <figure class="md:flex bg-slate-100 rounded-xl p-8 md:p-0 dark:bg-slate-800 mt-5">
+        <img class="w-24 h-24 md:w-48 md:h-auto md:rounded-none rounded-full mx-auto" src="https://images.pexels.com/photos/104373/pexels-photo-104373.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1https://images.pexels.com/photos/4001296/pexels-photo-4001296.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" width="384" height="512" />
+          <div class="pt-6 md:p-8 text-center md:text-left space-y-4">
+            <blockquote>
+              <p class="text-lg font-medium">
+                “Tailwind CSS is the only framework that I've seen scale
+                on large teams. It’s easy to customize, adapts to any design,
+                and the build size is tiny.”
+              </p>
+            </blockquote>
+            <figcaption class="font-medium">
+              <div class="text-sky-500 dark:text-sky-400">
+                Sarah Dayan
+              </div>
+              <div class="text-slate-700 dark:text-slate-500">
+                Staff Engineer, Algolia
+              </div>
+            </figcaption>
+          </div>
+      </figure>
     </>
   )
 }
