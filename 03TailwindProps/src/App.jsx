744f3f6bf5ebaecc8b7865804854206f9f0047e0
Vikash Thakur <thakvikash622@gmail.com>
2024-03-16 23:20:13 +0530

    Learn about props and also utilize in the project

--- 03TailwindProps/src/App.jsx
+++ 03TailwindProps/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Card from './components/Card'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,26 +10,8 @@ function App() {
   return (
     <>
       <h2 className='bg-green-500 text-black p-4 rounded-xl'>Welcome to my Webpage:</h2>
-      <figure class="md:flex bg-slate-100 rounded-xl p-8 md:p-0 dark:bg-slate-800 mt-5">
-        <img class="w-24 h-24 md:w-48 md:h-auto md:rounded-none rounded-full mx-auto" src="https://images.pexels.com/photos/104373/pexels-photo-104373.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1https://images.pexels.com/photos/4001296/pexels-photo-4001296.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" width="384" height="512" />
-          <div class="pt-6 md:p-8 text-center md:text-left space-y-4">
-            <blockquote>
-              <p class="text-lg font-medium">
-                “Tailwind CSS is the only framework that I've seen scale
-                on large teams. It’s easy to customize, adapts to any design,
-                and the build size is tiny.”
-              </p>
-            </blockquote>
-            <figcaption class="font-medium">
-              <div class="text-sky-500 dark:text-sky-400">
-                Sarah Dayan
-              </div>
-              <div class="text-slate-700 dark:text-slate-500">
-                Staff Engineer, Algolia
-              </div>
-            </figcaption>
-          </div>
-      </figure>
+      <Card name="vikash" />
+      <Card name="Sharma" />
     </>
   )
 }
