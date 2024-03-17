e108e149fc57a0eb16ccd3d3fc50a3b2583e6e77
Vikash Thakur <thakvikash622@gmail.com>
2024-03-17 14:19:28 +0530

    Done with this project

--- 04bgChanger/src/App.jsx
+++ 04bgChanger/src/App.jsx
@@ -2,18 +2,25 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [color, setColour] = useState("olive")
+
 
   return (
     <>
-      <div className="mainContainer">
-        <div className="card bg-red-600" >Red</div>
-        <div className="card bg-green-600">Green</div>
-        <div className="card bg-orange-600">Orange</div>
-        <div className="card bg-purple-600">Purple</div>
-        <div className="card bg-gray-600">Gray</div>
-        <div className="card bg-yellow-600">Yellow</div>
-        <div className="card bg-blue-600">Blue</div>
+      <div className="mainContainer w-full h-screen duration-200"
+        style={{ backgroundColor: color }}>
+        <div className="innerContainer bg-white">
+          <div className="card bg-red-600" onClick={() => setColour("red")}>Red</div>
+          <div className="card bg-green-600" onClick={() => setColour("green")}>Green</div>
+          <div className="card bg-orange-600" onClick={() => setColour("orange")}>Orange</div>
+          <div className="card bg-purple-600" onClick={() => setColour("purple")}>Purple</div>
+          <div className="card bg-gray-600" onClick={() => setColour("gray")}>Gray</div>
+          <div className="card bg-yellow-600" onClick={() => setColour("yellow")}>Yellow</div>
+          <div className="card bg-blue-600" onClick={() => setColour("blue")}>Blue</div>
+          <div className="card bg-black text-white" onClick={() => setColour("black")}>Black</div>
+          <div className="card bg-pink-600" onClick={() => setColour("pink")}>Pink</div>
+          <div className="card bg-indigo-600" onClick={() => setColour("indigo")}>Indigo</div>
+        </div>
       </div>
     </>
   )
