106f4ff55f058ecc6c197849df9cd1eec542338b
Vikash Thakur <thakvikash622@gmail.com>
2024-03-18 15:56:31 +0530

    Created a function that will generate the password

--- 05PasswordGenerator/src/App.jsx
+++ 05PasswordGenerator/src/App.jsx
@@ -1,8 +1,33 @@
-import { useState } from 'react'
+import { useCallback, useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [length, setLength] = useState(8);
+  const [numberallowed, setNumberallowed] = useState(false);
+  const [characterallowed, setCharacterallowed] = useState(false);
+  const [password, setPassword] = useState("");
+
+
+  const passwordGenerator = useCallback(() => {
+    const pass = "";
+    const str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    if(numberallowed) {
+      str += "0123456789"
+    }
+    if(characterallowed) {
+      str += "!@#$%^&*~*-+/";
+    }
+
+    for(let i=1; i<length; i++) {
+      let char = math.floor(math.random() * str.length + 1);
+      pass = str.charAt(char);
+    }
+
+    setPassword(pass);
+
+
+    
+  }, [length, numberallowed, characterallowed, setPassword]);
 
   return (
     <>
@@ -15,13 +40,16 @@ function App() {
 
         <div className="lower">
           <div className="first">
-            <input type="range" /> Length(8)
+            <input type="range" /> 
+            <label>Length ({length})</label>
           </div>
           <div className="second">
-            <input type="checkbox" className='char' /> Numbers
+            <input type="checkbox" className='char' /> 
+            <label>Numbers</label>
           </div>
           <div className="third">
-            <input type="checkbox" className='char'/> Characters
+            <input type="checkbox" className='char'/>
+            <label>Characters</label>
           </div>
         </div>
       </div>
