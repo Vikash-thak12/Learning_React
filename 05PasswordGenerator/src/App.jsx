0d6406ea48ea1f5d4688be1288672ae3f4020024
Vikash Thakur <thakvikash622@gmail.com>
2024-03-18 15:43:13 +0530

    Basic styling and strucutre of the webpage

--- 05PasswordGenerator/src/App.jsx
+++ 05PasswordGenerator/src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
@@ -8,7 +6,25 @@ function App() {
 
   return (
     <>
-     
+      <div className="main-container">
+        <h1>Password Generator</h1>
+        <div className="inputs">
+          <input type="text" readOnly placeholder='password' />
+          <button className='btn'>Copy</button>
+        </div>
+
+        <div className="lower">
+          <div className="first">
+            <input type="range" /> Length(8)
+          </div>
+          <div className="second">
+            <input type="checkbox" className='char' /> Numbers
+          </div>
+          <div className="third">
+            <input type="checkbox" className='char'/> Characters
+          </div>
+        </div>
+      </div>
     </>
   )
 }
