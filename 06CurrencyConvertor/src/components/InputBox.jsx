8a490988ee80ae1f23541168c305d9ae12975070
Vikash Thakur <thakvikash622@gmail.com>
2024-03-27 13:15:56 +0530

    Import a new hook named useID

--- 06CurrencyConvertor/src/components/InputBox.jsx
+++ 06CurrencyConvertor/src/components/InputBox.jsx
@@ -1,3 +1,4 @@
+import React, {useId} from "react";
 
 function InputBox({
     label,
@@ -10,16 +11,15 @@ function InputBox({
     currencyDisable = false,
     className = "",
 }) {
-
-
+    const amountInputId = useId();
     return (
         <div className={`bg-white p-3 rounded-lg text-sm flex ${className}`}>
             <div className="w-1/2">
-                <label className="text-black/40 mb-2 inline-block">
+                <label className="text-black/40 mb-2 inline-block" htmlFor="amountInputId">
                     {label}
                 </label>
                 <input
-
+                    id="amountInputId"
                     className="outline-none w-full bg-transparent py-1.5"
                     type="number"
                     placeholder="Amount"
