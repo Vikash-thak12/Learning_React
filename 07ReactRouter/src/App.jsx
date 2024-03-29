c54b537b59c63489ee70196a897c715af0713c67
Vikash Thakur <thakvikash622@gmail.com>
2024-03-29 17:25:25 +0530

    Getting components work with react-router

--- 07ReactRouter/src/App.jsx
+++ 07ReactRouter/src/App.jsx
@@ -1,5 +1,7 @@
 import './App.css'
+import Footer from './components/Footer/Footer'
 import Header from './components/Header/Header'
+import Home from './components/Home/Home'
 
 function App() {
 
@@ -7,6 +9,8 @@ function App() {
     <>
     <h1 className='bg-red-700 text-white text-3xl rounded-xl p-5'>This is vikash Thakur</h1>
     <Header />
+    <Home />
+    <Footer />
     </>
   )
 }
