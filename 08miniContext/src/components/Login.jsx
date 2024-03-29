b31072d612ef1ef93a4990f31414c74d34459887
Vikash Thakur <thakvikash622@gmail.com>
2024-03-30 00:23:40 +0530

    setting up the login page

--- 08miniContext/src/components/Login.jsx
+++ 08miniContext/src/components/Login.jsx
@@ -1,9 +1,19 @@
 import React, {useState, useContext} from 'react';
 
 const Login = () => {
+
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const handleSubmit = () => {
+
+    }
   return (
-    <>
-    </>
+    <div>
+        <h2>Login</h2>
+        <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} placeholder='Enter your name' />
+        <input type="text" value={password} onChange={(e) => setPassword(e.target.value)} placeholder='Enter your password' />
+        <button onClick={handleSubmit}>Submit</button>
+    </div>
   );
 };
 
