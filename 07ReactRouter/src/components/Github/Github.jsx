885e5938651923e6f4510664e75360e2c459668e
Vikash Thakur <thakvikash622@gmail.com>
2024-03-29 18:29:18 +0530

    Created a folder and link to the header section

---
+++ 07ReactRouter/src/components/Github/Github.jsx
@@ -0,0 +1,11 @@
+// import React from 'react';
+
+const Github = () => {
+  return (
+    <>
+    <div className="bg-gray-600 text-center text-white text-3xl p-5"> Github Followers</div>
+    </>
+  );
+};
+
+export default Github;
