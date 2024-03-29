6116c1e0935cb4f5b6793711b72b79768d67fdfb
Vikash Thakur <thakvikash622@gmail.com>
2024-03-30 00:14:49 +0530

    imported usercontextprovider

--- 08miniContext/src/App.jsx
+++ 08miniContext/src/App.jsx
@@ -1,14 +1,15 @@
 // import { useState } from 'react'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
+import UserContextProvider from './context/UserContextProvider'
 import './App.css'
 
 function App() {
 
   return (
-    <>
-      <h1>This is the webpage for the miniContext</h1>
-    </>
+    <UserContextProvider>
+      <h1 className='bg-red-600 text-3xl p-5 rounded-2xl'>This is the webpage for the miniContext</h1>
+    </UserContextProvider>
   )
 }
 
