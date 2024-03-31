000be54e63240bd57c9d2eb3bbf497701849eddb
Vikash Thakur <thakvikash622@gmail.com>
2024-03-31 16:51:45 +0530

    Added values and methods in todocontext

--- 10TodoList/src/contexts/TodoContext.js
+++ 10TodoList/src/contexts/TodoContext.js
@@ -1,10 +1,24 @@
 import { useContext } from "react";
 import { createContext } from "react";
 
-export const TodoContext = createContext();
+export const TodoContext = createContext({
+    todos: [
+        {
+            id: 1,
+            todo: "todo msg",
+            completed: false,
+
+        }
+    ],
+
+    todoadd: (todo) => {},
+    todoupdate: (id, todo) => {},
+    tododelete: (id) => {},
+    togglecomplete: (id) => {}
+});
 
 export const UseContext = () => {
     return useContext(TodoContext);
 }
 
-export const TodoProvider = TodoContext.Provider();
+export const TodoProvider = TodoContext.Provider;
