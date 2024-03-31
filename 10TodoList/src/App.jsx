c90090b79f272adc6bec79d9a28de34d40999f2a
Vikash Thakur <thakvikash622@gmail.com>
2024-03-31 17:27:01 +0530

    Add a functionality for adding todo in the array:

--- 10TodoList/src/App.jsx
+++ 10TodoList/src/App.jsx
@@ -6,8 +6,12 @@ function App() {
 
   const [todos, setTodos] = useState([])
 
+  const  todoadd = (todo) => {
+    setTodos( (prev) => [{id: Date.now(), ...todo},...prev])
+  }
+
   return (
-    <TodoProvider>
+    <TodoProvider value={{todos, todoadd, tododelete, todoupdate, togglecomplete}}>
       <div className="bg-[#31496c] min-h-screen">
         <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
           <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
