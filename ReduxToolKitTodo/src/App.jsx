330c0b480611a7831f2292442cb797dfca422ce2
Vikash Thakur <thakvikash622@gmail.com>
2024-04-06 16:33:27 +0530

    Basic Changes

--- ReduxToolKitTodo/src/App.jsx
+++ ReduxToolKitTodo/src/App.jsx
@@ -1,10 +1,19 @@
+import { useState } from 'react'
 import './App.css'
+import Navbar from './components/Navbar'
 
 function App() {
 
+  const [count, setCount] = useState(0)
+  const counter = () => {
+    setCount(count+1)
+  }
+
   return (
     <>
+    <Navbar count = {count} />
      <h1>This is vikash Thakur</h1>
+     <button onClick={counter}>Click here: {count}</button>
     </>
   )
 }
---
+++ ReduxToolKitTodo/src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Navbar = ({count}) => {
+  return (
+    <>
+    <h2>The value of Navbar is: {count}</h2>
+    </>
+  );
+};
+
+export default Navbar;
