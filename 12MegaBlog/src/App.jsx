cf10761aebec0065c1867551168f1e7add1610ab
Vikash Thakur <thakvikash622@gmail.com>
2024-04-14 23:35:15 +0530

    Fix the environmental variable

--- 12MegaBlog/src/App.jsx
+++ 12MegaBlog/src/App.jsx
@@ -1,11 +1,14 @@
 import './App.css'
 
 const App = () => {
+
+  console.log(import.meta.env.VITE_APPWRITE_URL);
+  
   return (
     <>
-    <h1>Baisc Blog app with Appwrite</h1>
+    <h1>Building the Blog app</h1>
     </>
   );
-};
+};  
 
 export default App;
