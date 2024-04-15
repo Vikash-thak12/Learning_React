9f33569a12136a946fa4116328c6790d72bba04e
Vikash Thakur <thakvikash622@gmail.com>
2024-04-15 15:28:11 +0530

    Wokring on the createpost functionality

---
+++ 12MegaBlog/src/appwrite/mainconfig.js
@@ -0,0 +1,40 @@
+import config from "../config/config";
+import { Client, ID, Databases, Storage } from "appwrite";
+
+export class Service {
+    client = new Client();
+    databases;
+    storage;
+
+    constructor() {
+        this.client
+            .setEndpoint(config.appwriteurl)
+            .setProject(config.appwriteProjectId)
+        this.databases = new Databases(this.client);
+        this.storage  - new Storage(this.client);
+    }
+
+
+    async createPost ({title, content, featuredImage, status, userId, slug}) {
+        try {
+            return await this.databases.createDocument(
+
+                    config.appwriteDatabaseId,
+                    config.appwriteCollectionId,
+                    slug,
+                    {
+                        title, 
+                        content,
+                        featuredImage, 
+                        status,
+                        userId, 
+                    }
+            )
+        } catch (error) {
+            console.log("Create post Error: ", error);
+        }
+    }
+}
+
+const service = new Service();
+export default service;
