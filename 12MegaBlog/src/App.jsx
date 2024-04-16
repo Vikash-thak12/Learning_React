6bbf74a64323ededb6a8d8ae5150350c214d5650
Vikash Thakur <thakvikash622@gmail.com>
2024-04-16 22:35:32 +0530

    working on the functionality of user status

--- 12MegaBlog/src/App.jsx
+++ 12MegaBlog/src/App.jsx
@@ -1,12 +1,25 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css'
 import { useDispatch } from 'react-redux';
-import authservice from './appwrite/auth'
+import authservice from './appwrite/auth';
+import { login, logout} from './store/authSlice'
 
 const App = () => {
 
   const [loading, setLoading] = useState(false)
   const dispatch = useDispatch();
+  
+  useEffect( () => {
+    authservice.getCurrentUser()
+    .then( (userData) => {
+      if( userData) {
+        dispatch(login({userData}))
+      } else {
+        dispatch(logout())
+      }
+    })
+    .finally( () => setLoading(false))
+  }, [])
   
   return (
     <>
