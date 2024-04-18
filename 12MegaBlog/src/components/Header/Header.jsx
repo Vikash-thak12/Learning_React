f9f37703f19032aaee0f9823cd64eac925a38590
Vikash Thakur <thakvikash622@gmail.com>
2024-04-18 09:18:50 +0530

    New file created and exported through index

--- 12MegaBlog/src/components/Header/Header.jsx
+++ 12MegaBlog/src/components/Header/Header.jsx
@@ -1,9 +1,22 @@
+import { Container, Logo, LogoutBtn} from '../index'
+import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+
+
 const Header = () => {
+  const authStatus = useSelector( (state) => state.auth.status)
+  const navigate = useNavigate();
+
+  const navItems = [
+    {
+      name: 'Home',
+      slug: '/',
+      active: true
+    },
+  ]
   return (
-    <>
-    <h1>Header
-    </h1>
-    </>
+    <div>Header</div>
   );
 };
 
