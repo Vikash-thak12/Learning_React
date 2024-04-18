dbe0ca3376b37717a256aef87eb688069259d571
Vikash Thakur <thakvikash622@gmail.com>
2024-04-18 16:12:10 +0530

    working on header

--- 12MegaBlog/src/components/Header/Header.jsx
+++ 12MegaBlog/src/components/Header/Header.jsx
@@ -1,11 +1,11 @@
-import { Container, Logo, LogoutBtn} from '../index'
+import { Container, Logo, LogoutBtn } from '../index'
 import { Link } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 
 
 const Header = () => {
-  const authStatus = useSelector( (state) => state.auth.status)
+  const authStatus = useSelector((state) => state.auth.status)
   const navigate = useNavigate();
 
   const navItems = [
@@ -27,7 +27,7 @@ const Header = () => {
     {
       name: "All Posts",
       slug: "/All-Posts",
-      active: !authStatus,
+      active: authStatus,
     },
     {
       name: "Add Post",
@@ -36,7 +36,34 @@ const Header = () => {
     }
   ]
   return (
-    <div>Header</div>
+    <div className='py-6 shadow bg-gray-500'>
+      <Container>
+        <nav className='flex'>
+          <div className='mr-4'>
+            <Link to={'/'}>
+              <Logo width='70px' />
+            </Link>
+          </div>
+          <ul className='flex ml-auto'>
+            {navItems.map( (item) => 
+            item.active ? (
+              <li key={item.name}>
+                <button
+                onClick={() => navigate(item.slug)}
+                className='inline-block px-6 py-2 duration-200 hover:bg-blue-100 rounded-full'
+                >{item.name}</button>
+              </li>
+            ) : null
+          )}
+          {authStatus && (
+            <li>
+              <LogoutBtn />
+            </li>
+          )}
+          </ul>
+        </nav>
+      </Container>
+    </div>
   );
 };
 
