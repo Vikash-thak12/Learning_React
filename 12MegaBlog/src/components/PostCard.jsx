9a7fb603fcf00abe5c8ec64b592e5a46d48adb72
Vikash Thakur <thakvikash622@gmail.com>
2024-04-19 00:24:36 +0530

    Done with postcard

--- 12MegaBlog/src/components/PostCard.jsx
+++ 12MegaBlog/src/components/PostCard.jsx
@@ -7,8 +7,9 @@ const PostCard = ({$id, title, featuredImage}) => {
     <Link to={`/post/${$id}`}>
         <div className='w-full bg-gray-100 rounded-xl p-4'>
             <div className='w-full justify-center mb-4'>
-                
+                <img src={appwriteService.getFilePreview(featuredImage)} alt={title} className='rounded-xl' />
             </div>
+            <h2 className='text-xl font-bold'>{title}</h2>
         </div>
     </Link>
   );
