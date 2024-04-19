72eee372af0473e03e062ee804eec06ee88b5d13
Vikash Thakur <thakvikash622@gmail.com>
2024-04-19 23:59:31 +0530

    Created necessary states

--- 12MegaBlog/src/components/AuthLayout.jsx
+++ 12MegaBlog/src/components/AuthLayout.jsx
@@ -2,12 +2,15 @@ import React, {useEffect, useState} from 'react'
 import {useSelector} from 'react-redux'
 import {useNavigate} from 'react-router-dom'
 
-const AuthLayout = () => {
+export default function Protected ( {children, authentication = true}) {
+
+    const navigate = useNavigate()
+    const [loader, setLoader] = useState(true)
+    const authStatus = useSelector(state => state.auth.status)
+    
   return (
     <div>
     AuthLayout
     </div>
   );
-};
-
-export default AuthLayout;
+}
