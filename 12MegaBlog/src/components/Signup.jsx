25adb01bd99aff83347614d29bff6434e24a466b
Vikash Thakur <thakvikash622@gmail.com>
2024-04-19 23:40:52 +0530

    created necessary states

--- 12MegaBlog/src/components/Signup.jsx
+++ 12MegaBlog/src/components/Signup.jsx
@@ -1,15 +1,22 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import authService from '../appwrite/auth'
-import {Link ,useNavigate} from 'react-router-dom'
-import {login} from '../store/authSlice'
-import {Button, Input, Logo} from './index.js'
-import {useDispatch} from 'react-redux'
-import {useForm} from 'react-hook-form'
+import { Link, useNavigate } from 'react-router-dom'
+import { login } from '../store/authSlice'
+import { Button, Input, Logo } from './index.js'
+import { useDispatch } from 'react-redux'
+import { useForm } from 'react-hook-form'
 
 const Signup = () => {
+
+  const navigate = useNavigate()
+  const [error, setError] = useState("")
+  const dispatch = useDispatch()
+  const { register, handleSubmit } = useForm()
+
+
   return (
     <div>
-    Signup
+      Signup
     </div>
   );
 };
