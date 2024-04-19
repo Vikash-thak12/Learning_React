450a4180de660460836fc83ed0a0a00e220136e5
Vikash Thakur <thakvikash622@gmail.com>
2024-04-19 23:56:11 +0530

    Imported necessary components

---
+++ 12MegaBlog/src/components/AuthLayout.jsx
@@ -0,0 +1,13 @@
+import React, {useEffect, useState} from 'react'
+import {useSelector} from 'react-redux'
+import {useNavigate} from 'react-router-dom'
+
+const AuthLayout = () => {
+  return (
+    <div>
+    AuthLayout
+    </div>
+  );
+};
+
+export default AuthLayout;
