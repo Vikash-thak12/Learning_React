db3a3a3fc54dfa7c352bc60637ed83c560d3b781
Vikash Thakur <thakvikash622@gmail.com>
2024-04-21 01:56:09 +0530

    Imported and exported signup compo from index

--- 12MegaBlog/src/components/index.js
+++ 12MegaBlog/src/components/index.js
@@ -1,21 +1,26 @@
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
 import Container from "./Container/Container";
+import PostForm from "./Post-Form/PostForm";
 import Logo from "./Logo";
 import LogoutBtn from "./Header/LogoutBtn";
 import Button from "./Button";
 import Select from './Select'
 import PostCard from './PostCard'
 import RTE from "./RTE";
+import Signup from './Signup'
+
 
 export {
     Header,
     Footer,
     Container,
+    PostForm,
     Logo,
     LogoutBtn,
     Button,
     Select,
     PostCard,
-    RTE
+    RTE,
+    Signup
 }
