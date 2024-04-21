661ca228b3ef1c29e524d4067c92d684d1d9cf2b
Vikash Thakur <thakvikash622@gmail.com>
2024-04-21 14:47:04 +0530

    Created two pages

--- 12MegaBlog/src/pages/Addpost.jsx
+++ 12MegaBlog/src/pages/Addpost.jsx
@@ -1,33 +1,14 @@
-import React, { useEffect, useState } from 'react';
-import { Container, PostCard, PostForm } from '../components';
-import appwritreService from '../appwrite/mainconfig'
+import React from 'react'
+import { Container, PostForm } from '../components'
 
-const Addpost = () => {
-
-
-  const [posts, setPosts] = useState([])
-  useEffect(() => { })
-
-  appwritreService.getPost([]).then((posts) => {
-    if (posts) {
-      setPosts(posts.documents)
-    }
-  })
-
-
+function AddPost() {
   return (
     <div className='py-8'>
-      <Container >
-        <div className='flex flex-wrap'>
-          {posts.map((post) => (
-            <div key={post.$id} className='p-2 w-1/4' >
-              <PostCard post={post} />
-            </div>
-          ))}
-        </div>
-      </Container>
+        <Container>
+            <PostForm />
+        </Container>
     </div>
-  );
-};
+  )
+}
 
-export default Addpost; 
+export default AddPost
---
+++ 12MegaBlog/src/pages/AllPosts.jsx
@@ -0,0 +1,33 @@
+import React, { useEffect, useState } from 'react';
+import { Container, PostCard } from '../components';
+import appwritreService from '../appwrite/mainconfig'
+
+const Addpost = () => {
+
+
+  const [posts, setPosts] = useState([])
+  useEffect(() => { })
+
+  appwritreService.getPost([]).then((posts) => {
+    if (posts) {
+      setPosts(posts.documents)
+    }
+  })
+
+
+  return (
+    <div className='py-8'>
+      <Container >
+        <div className='flex flex-wrap'>
+          {posts.map((post) => (
+            <div key={post.$id} className='p-2 w-1/4' >
+              <PostCard post={post} />
+            </div>
+          ))}
+        </div>
+      </Container>
+    </div>
+  );
+};
+
+export default Addpost; 
